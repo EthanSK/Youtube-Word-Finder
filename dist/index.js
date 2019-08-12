4dcefd2e8290b5e0dd5e998f65f2cd6f1267d040
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-08-12 23:26:07 +0100

    subs

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,35 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const child_process_1 = require("child_process");
-const ffmpeg_1 = require("@ffmpeg-installer/ffmpeg");
-const ffmpeg = child_process_1.spawn(ffmpeg_1.path, [
-    "-i",
-    "./playground/testVideo.mp4",
-    "./playground/testFfmpegOut.mp4"
-]);
-ffmpeg.stdout.setEncoding("utf8");
-ffmpeg.stdout.on("data", function (data) {
-    console.log("stdout data: ", data);
+const youtube_dl_1 = __importDefault(require("youtube-dl"));
+// const ffmpeg = spawn(ffmpegPath, [
+//   "-i",
+//   "./playground/testVideo.mp4",
+//   "./playground/testFfmpegOut.mp4"
+// ])
+// ffmpeg.stdout.setEncoding("utf8")
+// ffmpeg.stdout.on("data", function(data) {
+//   console.log("stdout data: ", data)
+// })
+// ffmpeg.stderr.setEncoding("utf8")
+// ffmpeg.stderr.on("data", function(data) {
+//   console.log("stderr data: ", data)
+// })
+// ffmpeg.on("close", (code, signal) => {
+//   console.log(code, signal)
+// })
+const path = process.cwd() + "/" + "subs";
+console.log("path:", path);
+youtube_dl_1.default.getSubs("https://www.youtube.com/watch?v=Pc6rnTFjJZI", {
+    auto: false,
+    all: false,
+    lang: "en",
+    cwd: path
+}, (err, files) => {
+    if (err)
+        console.error("error subs:", err);
+    console.log("subtitle files downloaded:", files);
 });
-ffmpeg.stderr.setEncoding("utf8");
-ffmpeg.stderr.on("data", function (data) {
-    console.log("stderr data: ", data);
-});
-ffmpeg.on("close", (code, signal) => {
-    console.log(code, signal);
-});
+// youtube-dl --write-auto-sub https://www.youtube.com/watch?v=Pc6rnTFjJZI --skip-download -o playground/subs/subtest
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,42 @@
 import { spawn } from "child_process"
 import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg"
+import youtubedl from "youtube-dl"
 
-const ffmpeg = spawn(ffmpegPath, [
-  "-i",
-  "./playground/testVideo.mp4",
-  "./playground/testFfmpegOut.mp4"
-])
+// const ffmpeg = spawn(ffmpegPath, [
+//   "-i",
+//   "./playground/testVideo.mp4",
+//   "./playground/testFfmpegOut.mp4"
+// ])
 
-ffmpeg.stdout.setEncoding("utf8")
+// ffmpeg.stdout.setEncoding("utf8")
 
-ffmpeg.stdout.on("data", function(data) {
-  console.log("stdout data: ", data)
-})
+// ffmpeg.stdout.on("data", function(data) {
+//   console.log("stdout data: ", data)
+// })
 
-ffmpeg.stderr.setEncoding("utf8")
-ffmpeg.stderr.on("data", function(data) {
-  console.log("stderr data: ", data)
-})
+// ffmpeg.stderr.setEncoding("utf8")
+// ffmpeg.stderr.on("data", function(data) {
+//   console.log("stderr data: ", data)
+// })
 
-ffmpeg.on("close", (code, signal) => {
-  console.log(code, signal)
-})
+// ffmpeg.on("close", (code, signal) => {
+//   console.log(code, signal)
+// })
+const path = process.cwd() + "/" + "subs"
+
+console.log("path:", path)
+youtubedl.getSubs(
+  "https://www.youtube.com/watch?v=Pc6rnTFjJZI",
+  {
+    auto: false,
+    all: false,
+    lang: "en",
+    cwd: path
+  },
+  (err, files) => {
+    if (err) console.error("error subs:", err)
+    console.log("subtitle files downloaded:", files)
+  }
+)
+
+// youtube-dl --write-auto-sub https://www.youtube.com/watch?v=Pc6rnTFjJZI --skip-download -o playground/subs/subtest
