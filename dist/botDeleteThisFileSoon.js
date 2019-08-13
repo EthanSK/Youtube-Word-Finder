b05779f97522e3f451f02bdf511bd8134efd0076
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2019-08-13 17:35:14 +0100

    testing youtube-dl

--- dist/botDeleteThisFileSoon.js
+++ dist/botDeleteThisFileSoon.js
@@ -1,36 +1,46 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const youtube_dl_1 = __importDefault(require("youtube-dl"));
-const path_1 = __importDefault(require("path"));
+const child_process_1 = require("child_process");
+const ffmpeg_1 = require("@ffmpeg-installer/ffmpeg");
 // const ffmpeg = spawn(ffmpegPath, [
 //   "-i",
 //   "./playground/testVideo.mp4",
 //   "./playground/testFfmpegOut.mp4"
 // ])
-// ffmpeg.stdout.setEncoding("utf8")
-// ffmpeg.stdout.on("data", function(data) {
-//   console.log("stdout data: ", data)
-// })electro
-// ffmpeg.stderr.setEncoding("utf8")
-// ffmpeg.stderr.on("data", function(data) {
-//   console.log("stderr data: ", data)
-// })
-// ffmpeg.on("close", (code, signal) => {
-//   console.log(code, signal)
-// })
-const pathsubs = path_1.default.join(process.cwd(), "subs");
-console.log("path:", pathsubs);
-youtube_dl_1.default.getSubs("https://www.youtube.com/watch?v=Pc6rnTFjJZI", {
-    auto: true,
-    all: false,
-    lang: "en",
-    cwd: pathsubs
-}, (err, files) => {
-    if (err)
-        console.error("error subs:", err);
-    console.log("subtitle files downloaded:", files);
+const ffmpeg = child_process_1.spawn(ffmpeg_1.path, [
+    "-ss",
+    "0",
+    "-i",
+    "./playground/testVideo.mp4",
+    "./playground/testFfmpegOut.mp4",
+    "-t",
+    "1"
+]);
+ffmpeg.stdout.setEncoding("utf8");
+ffmpeg.stdout.on("data", function (data) {
+    console.log("stdout data: ", data);
 });
+ffmpeg.stderr.setEncoding("utf8");
+ffmpeg.stderr.on("data", function (data) {
+    console.log("stderr data: ", data);
+});
+ffmpeg.on("close", (code, signal) => {
+    console.log(code, signal);
+});
+// const pathsubs = path.join(process.cwd(), "subs")
+// console.log("path:", pathsubs)
+// youtubedl.getSubs(
+//   "https://www.youtube.com/watch?v=Pc6rnTFjJZI",
+//   {
+//     auto: true,
+//     all: false,
+//     lang: "en",
+//     cwd: pathsubs
+//   },
+//   (err, files) => {
+//     if (err) console.error("error subs:", err)
+//     console.log("subtitle files downloaded:", files)
+//   }
+// )
 // youtube-dl --write-auto-sub https://www.youtube.com/watch?v=Pc6rnTFjJZI --skip-download -o playground/subs/subtest
+//ffmpeg -ss 0 -i $(youtube-dl -f 22 -g 'https://www.youtube.com/watch?v=aLFgbkN3-hM') -t 5 lol.mp4
--- src/botDeleteThisFileSoon.ts
+++ src/botDeleteThisFileSoon.ts
@@ -9,35 +9,47 @@ import path from "path"
 //   "./playground/testFfmpegOut.mp4"
 // ])
 
-// ffmpeg.stdout.setEncoding("utf8")
+const ffmpeg = spawn(ffmpegPath, [
+  "-ss",
+  "0",
+  "-i",
+  "./playground/testVideo.mp4",
+  "./playground/testFfmpegOut.mp4",
+  "-t",
+  "1"
+])
 
-// ffmpeg.stdout.on("data", function(data) {
-//   console.log("stdout data: ", data)
-// })electro
+ffmpeg.stdout.setEncoding("utf8")
 
-// ffmpeg.stderr.setEncoding("utf8")
-// ffmpeg.stderr.on("data", function(data) {
-//   console.log("stderr data: ", data)
-// })
+ffmpeg.stdout.on("data", function(data) {
+  console.log("stdout data: ", data)
+})
 
-// ffmpeg.on("close", (code, signal) => {
-//   console.log(code, signal)
-// })
-const pathsubs = path.join(process.cwd(), "subs")
+ffmpeg.stderr.setEncoding("utf8")
+ffmpeg.stderr.on("data", function(data) {
+  console.log("stderr data: ", data)
+})
 
-console.log("path:", pathsubs)
-youtubedl.getSubs(
-  "https://www.youtube.com/watch?v=Pc6rnTFjJZI",
-  {
-    auto: true,
-    all: false,
-    lang: "en",
-    cwd: pathsubs
-  },
-  (err, files) => {
-    if (err) console.error("error subs:", err)
-    console.log("subtitle files downloaded:", files)
-  }
-)
+ffmpeg.on("close", (code, signal) => {
+  console.log(code, signal)
+})
+// const pathsubs = path.join(process.cwd(), "subs")
+
+// console.log("path:", pathsubs)
+// youtubedl.getSubs(
+//   "https://www.youtube.com/watch?v=Pc6rnTFjJZI",
+//   {
+//     auto: true,
+//     all: false,
+//     lang: "en",
+//     cwd: pathsubs
+//   },
+//   (err, files) => {
+//     if (err) console.error("error subs:", err)
+//     console.log("subtitle files downloaded:", files)
+//   }
+// )
 
 // youtube-dl --write-auto-sub https://www.youtube.com/watch?v=Pc6rnTFjJZI --skip-download -o playground/subs/subtest
+
+//ffmpeg -ss 0 -i $(youtube-dl -f 22 -g 'https://www.youtube.com/watch?v=aLFgbkN3-hM') -t 5 lol.mp4
